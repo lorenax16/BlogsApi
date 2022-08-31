9a9a4642b09228f6cebae7e07d7e40974e716b3a
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-08-31 15:16:01 -0300

    req 7

--- src/database/models/categories.js
+++
@@ -1,9 +0,0 @@
-const Categories = (sequelize, DataTypes) => {
-  const Categories = sequelize.define("Categorie", {
-    id: { type: DataTypes.INTEGER, primaryKey: true},
-    name: DataTypes.STRING,
-  });
-  return Categories;
-  };
-  
-  module.exports = Categories;
---
+++ src/database/models/category.js
@@ -0,0 +1,9 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: { type: DataTypes.INTEGER, primaryKey: true},
+    name: DataTypes.STRING,
+  });
+  return Category;
+  };
+  
+  module.exports = Category;
